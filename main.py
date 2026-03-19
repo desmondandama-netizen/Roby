9ed8b8621fc33ebd21d1bfce2006f3080d204e7f
desmond andama <desmondandama@gmail.com>
2026-03-19 23:39:46 +0300

    first version

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ day_of_the_week = "Monday"
 
 def suggestor(day_of_the_week):
     if day_of_the_week == "Monday":
-        print("Today we will eat, " , Food_1)
+        print("Today we will eat,", Food_1)
 
+def check_food_menu(food_menu):
+    if food_menu == "Food_3":
+        print("food_menu:", Food_3)
 
+# function calls
 suggestor("Monday")
+check_food_menu("Food_3")
 
 print("This is version 2")
