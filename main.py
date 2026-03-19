73296e59aa9cac5fab7b3e9bd98421f9d5c1257c
desmond andama <desmondandama@gmail.com>
2026-03-19 23:02:17 +0300

    first version

---
+++ main.py
@@ -0,0 +1,13 @@
+# "food menu"
+Food_1 = "Ugali"
+Food_2 = "Mushrooms"
+Food_3 = "Greens"
+
+day_of_the_week = "Monday"
+
+def suggestor(day_of_the_week):
+    if day_of_the_week == "Monday":
+        print("Today we will eat, " , Food_1)
+
+
+suggestor("Monday")
---
+++ readme.md
@@ -0,0 +1,5 @@
+Meal Suggestor app
+Store day of the week
+Food for that day
+
+Function to show us food
