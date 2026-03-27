d40d62a40157948c1b13b57e50ceacd113469624
desmond andama <desmondandama@gmail.com>
2026-03-27 19:39:01 +0300

    version 4

--- backend/desmond.py
+++ backend/desmond.py
@@ -9,3 +9,26 @@ def coffee_types():
 def milk_tea_types():
     return ["Bubble_milk_Tea", "Thai_Milk_Tea", "Matcha_Milk_Tea", "Taro_Milk_Tea"]
 
+
+def juice_types():
+    return ["Orange", "Apple", "Mangoe", "Pineapple", "Lemon"]
+
+#function call
+def get_drink_types(drink_category):
+    if drink_category == "tea":
+        return tea_types()
+    elif drink_category == "coffee":
+        return coffee_types()
+    elif drink_category == "milk_tea":
+        return milk_tea_types()
+    elif drink_category == "juice":
+        return juice_types()
+    else:
+        return "Invalid drink category"
+    
+#function call
+if __name__ == "__main__":
+    print(get_drink_types("tea"))
+    print(get_drink_types("coffee"))
+    print(get_drink_types("milk_tea"))
+    print(get_drink_types("juice"))
